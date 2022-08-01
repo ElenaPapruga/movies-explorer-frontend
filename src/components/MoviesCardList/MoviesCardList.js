a7be246d5916eeceaccc584496be3a6084ade3a3
Elena Pa <elenx@ya.ru>
2022-08-01 23:57:46 +0300

    Frontend-2-11

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -4,23 +4,65 @@ import React from "react";
 import "./MoviesCardList.css";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import { Route, Switch } from "react-router-dom";
+import PropTypes from 'prop-types';
 
-function MoviesCardList() {
+function MoviesCardList({ movies, changeFilterValue, removeMoviesFunction, newCard, handleMovieLike, addedNewCard, counterCard, addedMovie, removeMovie }) {
   return (
     <section className="movies-card-list">
       <div className="movies-card-list__elements">
-        <MoviesCard />
+        {movies?.slice(0, counterCard + newCard).map((movie, i) => {
+          return (
+            <MoviesCard
+              movie={movie}
+              handleMovieLike={handleMovieLike}
+              key={movie.movieId}
+              {...movie}
+              changeFilterValue={changeFilterValue}
+              removeMoviesFunction={removeMoviesFunction}
+              removeMovie={removeMovie}
+              addedMovie={addedMovie}
+            />
+          );
+        })}
       </div>
       <Switch>
-        <Route path="/movies">
-          <button className="movies-card-list__button" type="button" > Ещё </button>
+        <Route path='/movies'>movies-card-list__button-hidden
+          {movies?.length > counterCard + newCard && (
+            <button onClick={() => addedNewCard()} className='movies-card-list__button' type='button'>
+              {' '}
+              Ещё{' '}
+            </button>
+          )}
         </Route>
-        <Route path="/saved-movies">
-          <button className="movies-card-list__button movies-card-list__button-hidden" type="button" > Ещё </button>
+        <Route path='/saved-movies'>
+          {movies?.length > counterCard + newCard && (
+            <button onClick={
+              () => addedNewCard()} className='movies-card-list__button' type='button'>
+              {' '}
+              Ещё{' '}
+            </button>
+          )}
         </Route>
       </Switch>
     </section>
   );
 }
+
+MoviesCardList.propTypes = {
+  movie: PropTypes.shape({
+    movieId: PropTypes.number.isRequired,
+  }),
+  duration: PropTypes.number.isRequired,
+  image: PropTypes.string.isRequired,
+  nameRU: PropTypes.string.isRequired,
+  addedNewCard: PropTypes.func.isRequired,
+  handleMovieLike: PropTypes.func.isRequired,
+  changeFilterValue: PropTypes.func.isRequired,
+  removeMoviesFunction: PropTypes.func.isRequired,
+  counterCard: PropTypes.func.isRequired,
+  removeMovi: PropTypes.func.isRequired,
+  addedMovie: PropTypes.func.isRequired,
+}
+
 
 export default MoviesCardList;
--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,44 +1,130 @@
 // Register — компонент страницы регистрации
 
 import React from "react";
+import { useEffect, useState } from "react";
 import "./Register.css";
 import logo from "../../images/logo.svg";
-import { Link } from "react-router-dom";
+import { Redirect, Link } from "react-router-dom";
+import useFormValidation from "../../services/useFormValidation";
 
-function Register() {
+function Register(props) {
+  const { resetForm } = useFormValidation({});
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+  const [isValid, setIsValid] = useState(false);
+
+  const handleChangeName = (event) => {
+    const target = event.target;
+    const name = target.name;
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(event.target.closest('form').checkValidity());
+    setName(event.target.value);
+  }
+
+  function handleChangeEmail(event) {
+    const target = event.target;
+    const name = target.name;
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(event.target.closest('form').checkValidity());
+    setEmail(event.target.value);
+  }
+
+  //Проверка на валидность
+  function handleChangePassword(event) {
+    const target = event.target;
+    const name = target.name;
+    setErrors({ ...errors, [name]: target.validationMessage });
+    setIsValid(event.target.closest('form').checkValidity());
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    props.onRegister({ name, email, password });
+    resetForm();
+  }
+
+  useEffect(() => {
+    setName("");
+    setEmail("");
+    setPassword("");
+  }, []);
+
   return (
-    <section className="register">
-      <div className="register__wrapper">
-        <Link to="/">
-          <img className="register__logo" alt="Логотип" src={logo} />
-        </Link>
-        <h3 className="register__wellcome">Добро пожаловать!</h3>
-        <form className="register__form">
-          <label className="register__title">
-            Имя
-            <input
-              className="register__input" name="Имя" type="name" placeholder="Ваше имя" minLength="2" maxLength="40" required value='Виталий'/>
-          </label>
-          <label className="register__title">
-            E-mail
-            <input className="register__input" name="email" type="email" placeholder="Email" minLength="4" maxLength="40" required value='pochta@yandex.ru' />
-          </label>
-          <label className="register__title">
-            Пароль
-            <input className="register__input" name="password" type="password" placeholder="Пароль" minLength="2" maxLength="20" required />
-          </label>
-          <span className="register__input-error">Что-то пошло не так... </span>
-          <button type="submit" className="register__button">
-            Зарегистрироваться
-          </button>
-          <div className="register__text">
-            Уже зарегистрированы?
-            <Link className="register__link" to="/signin">Войти</Link>
+    <div>
+      {!props.loggedIn ? (
+        <section className="register">
+          <div className="register__wrapper">
+            <Link to="/">
+              <img className="register__logo" alt="Логотип" src={logo} />
+            </Link>
+            <h3 className="register__wellcome">Добро пожаловать!</h3>
+            <form className="register__form" onSubmit={handleSubmit}>
+              <label className="register__title">
+                Имя
+                <input
+                  className="register__input"
+                  pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$"
+                  name="Имя"
+                  type="name"
+                  placeholder="Ваше имя"
+                  required
+                  value={name || ""}
+                  onChange={handleChangeName}
+                />
+              </label>
+              <span className="register__input-error">{errors.name}</span>
+              <label className="register__title">
+                E-mail
+                <input
+                  className="register__input"
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  required
+                  value={email || ""}
+                  onChange={handleChangeEmail}
+                />
+              </label>
+              <span className="register__input-error">{errors.email}</span>
+              <label className="register__title">
+                Пароль
+                <input
+                  className="register__input"
+                  name="password"
+                  type="password"
+                  minLength="8"
+                  placeholder="Пароль"
+                  value={password || ""}
+                  onChange={handleChangePassword}
+                  required
+                  pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s).*$"
+                />
+              </label>
+              <span className="register__input-error">{errors.password}</span>
+              <button
+                type="submit"
+                className={`register__button ${!isValid && "register__button_disable"
+                  }`}
+                disabled={!isValid}
+              >
+                Зарегистрироваться
+              </button>
+              <div className="register__text">
+                Уже зарегистрированы?
+                <Link className="register__link" to="/signin">Войти</Link>
+              </div>
+            </form>
           </div>
-        </form>
-      </div>
-    </section>
+        </section>) : (
+        <Redirect to="./" />
+      )
+      }
+    </div >
   );
 }
+
 
 export default Register;
